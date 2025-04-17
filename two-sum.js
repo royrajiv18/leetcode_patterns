e56c99d12dc3aac380ab7d65e46224cf0584d784
Rajiv Roy <rajivpune18@gmail.com>
2025-04-17 13:15:38 +0530

    Create two-sum.js

---
+++ two-sum.js
@@ -0,0 +1,29 @@
+// The Two Sum problem states that given an array of integers, return indices of the two numbers such that they add up to a specific target. 
+// We can’t use the same element twice.
+// Ex - Given nums = [2, 7, 11, 15], target = 9,
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+// First approach - Brute Force
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+// Second approach - Optimized hash map approach
+function twoSum(nums, target) {
+let numObj = {};
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (numObj[complement] !== undefined) {
+      return [numObj[complement], i];
+    }
+    numObj[nums[i]] = i;
+  }
+}
+
