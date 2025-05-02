370d17fccf480124d568e5b1efd78012311cc45d
Rajiv Roy <rajivpune18@gmail.com>
2025-05-02 12:48:48 +0530

    remove element

--- remove_duplicates_array.js
+++ remove_duplicates_array.js
@@ -24,3 +24,28 @@ const removeDuplicates = (input)=> {
     return uniqueIndex
 }
 console.log(removeDuplicates(input)) //2
+
+/*
+Remove Element
+/*Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+*/
+
+const nums = [0,1,2,2,3,0,4,2];
+const val = 2
+
+const removeElement = (nums)=> {
+    let indexWithVal = 0
+    if(nums.length === 0) return indexWithVal;
+    
+    for(let i =0; i< nums.length; i++){
+        if(nums[i]!== val){
+            nums[indexWithVal] = nums[i];
+            indexWithVal++
+        }
+    }
+    return indexWithVal;
+    
+    
+}
+console.log(removeElement(nums)) // 5
