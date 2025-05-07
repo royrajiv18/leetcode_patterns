233adfda92d64f62ac17a4ef4edfca8379bc1a61
Rajiv Roy <rajivpune18@gmail.com>
2025-05-07 17:23:12 +0530

    palindrome number

---
+++ palindrome_number.js
@@ -0,0 +1,16 @@
+const input = 12321
+const isPalindrome = (input)=> {
+    if (input < 0) return false;
+    
+    let original = input;
+    let reversed =0;
+    
+    while(input > 0){
+        const digit = input%10;
+        reversed = (reversed * 10) + digit;
+        input = Math.floor(input/10);
+    }
+    return original === reversed
+}
+
+console.log(isPalindrome(input))
