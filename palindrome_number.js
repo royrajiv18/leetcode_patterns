1b99db1c44ae3a8bda886be5569fa7fa7b5254cb
Rajiv Roy <rajivpune18@gmail.com>
2025-05-08 22:02:15 +0530

    time complexity added

--- palindrome_number.js
+++ palindrome_number.js
@@ -7,10 +7,11 @@ const isPalindrome = (input)=> {
     
     while(input > 0){
         const digit = input%10;
-        reversed = (reversed * 10) + digit;
+        reversed = reversed * 10 + digit;
         input = Math.floor(input/10);
     }
     return original === reversed
 }
 
 console.log(isPalindrome(input))
+// O(log n), O(1)
